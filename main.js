16097cbacac8605c65c06688b772f39f939881ab
Cooper Dean <cooperdean2002@gmail.com>
2023-09-27 16:59:48 -0400

    glitch

--- main.js
+++ main.js
@@ -1,16 +1,18 @@
 import { default as seagulls } from './seagulls.js'
 import { default as Video    } from './video.js'
 import { default as Audio    } from './audio.js'
+import {Pane} from "https://cdn.jsdelivr.net/npm/tweakpane@4.0.1/dist/tweakpane.min.js";
 
+// Shader
 const shader = `
-@group(0) @binding(0) var<uniform> frame: f32;
-@group(0) @binding(1) var<uniform> res:   vec2f;
-@group(0) @binding(2) var<uniform> audio: vec3f;
-@group(0) @binding(3) var<uniform> mouse: vec3f;
-@group(0) @binding(4) var backSampler:    sampler;
-@group(0) @binding(5) var backBuffer:     texture_2d<f32>;
-@group(0) @binding(6) var videoSampler:   sampler;
-@group(1) @binding(0) var videoBuffer:    texture_external;
+@group(0) @binding(0) var<uniform> frame:                 f32;
+@group(0) @binding(1) var<uniform> res:                   vec2f;
+@group(0) @binding(2) var<uniform> speed:                 f32;
+@group(0) @binding(3) var<uniform> large_wave_strength:   f32;
+@group(0) @binding(4) var<uniform> small_wave_strength:   f32;
+@group(0) @binding(5) var<uniform> interference_strength: f32;
+@group(0) @binding(6) var videoSampler:                   sampler;
+@group(1) @binding(0) var videoBuffer:                    texture_external;
 
 @vertex 
 fn vs( @location(0) input : vec2f ) ->  @builtin(position) vec4f {
@@ -19,21 +21,24 @@ fn vs( @location(0) input : vec2f ) ->  @builtin(position) vec4f {
 
 @fragment 
 fn fs( @builtin(position) pos : vec4f ) -> @location(0) vec4f {
-  let p = pos.xy / res;
-  let vid = textureSampleBaseClampToEdge( videoBuffer, videoSampler, p );
-  let fb  = textureSample( backBuffer, backSampler, p );
+  var p = pos.xy / res;
   var color = vec3f(0.);
 
-  let q : vec2f = vec2f(fbm(p), fbm(p + vec2f(1.)));
-  let r : vec2f = vec2f(fbm(p + q + vec2f(1.7, 9.2) + 0.15*frame), fbm(p + q + vec2f(8.3, 2.8) + 0.125*frame));
-  let f : f32 = fbm(p + r);
+  let s = frame/20. * speed;
 
-  color = mix(vec3f(0.1, 0.6, 0.667), vec3f(0.667, 0.667, 0.5), clamp(f*f*4., 0., 1.));
-  color = mix(color, vec3f(0., 0., 0.16), clamp(length(q), 0., 1.));
-  color = mix(color, vec3f(0.667, 1., 1.), clamp(length(r.x), 0., 1.));
-  color *= f*f*f + 0.6*f*f + 0.5*f;
+  // Large waves
+  var n : f32 = max(0., noise(vec2f(s, p.y*0.3)) - 0.3) * large_wave_strength;
 
-  color.x = (vid * .05 + fb * .95).x;
+  // Small waves
+  n += (noise(vec2f(s*10., p.y*2.4)) - 0.5) * small_wave_strength;
+
+  // Apply noise
+  let xpos : f32 = p.x - n * n * 0.25;
+  color = textureSampleBaseClampToEdge(videoBuffer, videoSampler, vec2f(xpos, p.y)).rgb;
+
+  // Mix some random interference
+  color = mix(color, vec3f(random(vec2f(p.y*frame))), n*interference_strength);
+  
   return vec4f(color, 1.);
 }
 
@@ -55,46 +60,68 @@ fn noise( v : vec2f ) -> f32 {
   let u : vec2f = f*f*(3.-2.*f);
   return mix(a, b, u.x) + (c - a) * u.y * (1. - u.x) + (d - b) * u.x * u.y;
 }
-
-fn fbm( v : vec2f ) -> f32 {
-  var vp : vec2f = v;
-  var vf : f32 = 0.;
-  var a : f32 = 0.5;
-  let shift : vec2f = vec2f(100.);
-  let octaves : i32 = 5;
-
-  //rotate
-  let rot : mat2x2<f32> = mat2x2(cos(0.5), sin(0.5), -sin(0.5), cos(0.5));
-  for (var i = 0; i < octaves; i++) {
-    vf += a * noise(vp);
-    vp = rot * v * 2. + shift;
-    a *= 0.5;
-  }
-  return vf;
-}
 `
 
+// Shader loop
 async function main() {
-  let frame = 0
+  let frame = 0;
 
-  document.body.onclick = Audio.start
+  await Video.init();
 
-  await Video.init()
+  const sg = await seagulls.init();
+  
 
-  const sg = await seagulls.init()
+  // Tweakpane setup
+  const pane = new Pane();
+
+  const PARAMS = {
+    speed: 1,
+    large_waves: 1.5,
+    small_waves: 0.15,
+    interference: 0.3
+  }
+
+  pane.addBinding(
+    PARAMS, 
+    'speed', 
+    {min: 0, max: 3, step:0.1}
+  );
+
+  pane.addBinding(
+    PARAMS, 
+    'large_waves', 
+    {min: 0, max: 3, step:0.1}
+  );
+
+  pane.addBinding(
+    PARAMS, 
+    'small_waves', 
+    {min: 0, max: 0.5, step:0.005}
+  );
+
+  pane.addBinding(
+    PARAMS, 
+    'interference', 
+    {min: 0, max: 1, step:0.01}
+  );
 
   sg.uniforms({ 
     frame:0, 
     res:[window.innerWidth, window.innerHeight],
-    audio:[0,0,0],
-    mouse:[0,0,0],
+    speed:1,
+    large_wave_strength:1.5,
+    small_wave_strength:0.15,
+    interference_strength:0.3
   })
   .onframe( ()=> {
-    sg.uniforms.frame = frame++ 
-    sg.uniforms.audio = [ Audio.low, Audio.mid, Audio.high ]
+    sg.uniforms.frame = frame++;
+    sg.uniforms.speed = PARAMS.speed;
+    sg.uniforms.large_wave_strength = PARAMS.large_waves;
+    sg.uniforms.small_wave_strength = PARAMS.small_waves;
+    sg.uniforms.interference_strength = PARAMS.interference;
   })
   .textures([ Video.element ]) 
-  .render( shader, { uniforms: ['frame','res', 'audio', 'mouse' ] })
+  .render( shader, { uniforms: ['frame','res', 'speed', 'large_wave_strength', 'small_wave_strength', 'interference_strength'] })
   .run()
 }
 
